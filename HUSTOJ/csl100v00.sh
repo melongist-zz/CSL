edb7c3568e0b4c31db317083eaf8c9e8f57eb035
melongist <what_is_computer@msn.com>
2020-09-11 13:48:15 +0900

    Update csl100v00.sh

--- HUSTOJ/csl100v00.sh
+++ HUSTOJ/csl100v00.sh
@@ -1,17 +1,33 @@
+#kindeditor korean setting
 cd /home/judge/src/web/admin/
 sudo rm kindeditor.php
 wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/kindeditor.php
 sudo chown www-data kindeditor.php
 
+#front, rear, bann setting
 cd /home/judge/src/web/
 sudo rm submit.php
 wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/submit.php
 sudo chown www-data submit.php
 
+#QR codes removing + CSL link
 cd /home/judge/src/web/template/bs3/
 sudo rm js.php
 wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/js.php
 sudo chown www-data js.php
 
-echo "CSL100v00 ready"
+#copy all images to server
+cd /home/judge/src/web/upload
+sudo rm -rf *
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/csl100v00.tar.gz
+sudo tar zxvf csl100v00.tar.gz
+sudo rm csl100v00.tar.gz
+
+#install phpmyadmin
+#sudo apt install phpmyadmin
+#sudo ln -f -s /usr/share/phpmyadmin /home/judge/src/web/phpmyadmin
+#sudo mv /home/judge/src/web/phpmyadmin /home/judge/src/web/pma
+
+echo "HUSTOJ ver CSL100v00 completed!!"
+
 
