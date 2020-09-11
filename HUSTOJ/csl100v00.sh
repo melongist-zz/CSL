4623e6e31e18bcf624ffa28946c8b7ed38b6a97e
melongist <what_is_computer@msn.com>
2020-09-11 12:57:56 +0900

    Update csl100v00.sh

--- HUSTOJ/csl100v00.sh
+++ HUSTOJ/csl100v00.sh
@@ -1,3 +1,4 @@
 cd /home/judge/src/web/admin/
 rm kindeditor.php
-wget https://github.com/melongist/CSL/tree/master/HUSTOJ/kindeditor.php
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/kindeditor.php
+
