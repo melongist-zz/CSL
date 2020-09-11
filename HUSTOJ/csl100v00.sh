0998539f2aae05699d45d9a4f228f838b2f17bfb
melongist <what_is_computer@msn.com>
2020-09-11 23:14:22 +0900

    Update csl100v00.sh
    Ver 1.0

--- HUSTOJ/csl100v00.sh
+++ HUSTOJ/csl100v00.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt upgrade
 
@@ -24,7 +26,7 @@ cd /home/judge/src/web/upload
 sudo rm -rf *
 wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/csl100v00image.tar.gz
 sudo tar zxvf csl100v00image.tar.gz
-sudo rm csl100v00.tar.gz
+sudo rm csl100v00image.tar.gz
 
 #copy all data to server
 cd /home/judge/
@@ -55,8 +57,58 @@ sudo rm csl100v00jol.sql
 
 clear
 echo "Ver 2020.09.11"
-echo "CSL basic 100 problems install completed!!"
-echo "HUSTOJ admin ID : admin"
-echo "HUSTOJ admin PW : computerscience"
+echo "CSL 100 problems install completed!!"
+
+
+#db_info.inc.php edit
+cd /home/judge/src/web/include
+
+#OJ NAME
+INPUTS="n"
+OJNAME=""
+while [ ${INPUTS} = "n" ]; do
+  echo -n "OJ NAME?: "
+  read OJNAME
+  echo -n "OJ NAME : $OJNAME     OK?[y/n] "
+  read INPUTS
+done
+sed -i "s/OJ_NAME=\"HUSTOJ\"/OJ_NAME=\"${OJNAME}\"/" db_info.inc.php
+
+#for korean
+sed -i "s/OJ_LANG=\"en\"/OJ_LANG=\"ko\"/" db_info.inc.php
+
+#for OJ_VCODE
+sed -i "s/OJ_VCODE=false/OJ_VCODE=true/" db_info.inc.php
+
+#for unable to new register
+sed -i "s/OJ_REGISTER=true/OJ_REGISTER=false/" db_info.inc.php
+
+#for OJ_SHOW_DIFF
+INPUTS="n"
+while [ ${INPUTS} = "n" ]; do
+  echo -n "OJ_SHOW_DIFF=true    OK?[y/n] "
+  read INPUTS
+  if [ ${INPUTS} = "y" ]; then
+    sed -i "s/OJ_SHOW_DIFF=false/OJ_SHOW_DIFF=true/" db_info.inc.php
+  else  
+    echo -n "OJ_SHOW_DIFF=false   OK?[y/n] "
+    read INPUTS
+  fi
+done
+
+#for south korea timezone
+sed -i "s#//date_default_timezone_set(\"PRC\")#date_default_timezone_set(\"Asia\/Seoul\")#" db_info.inc.php
+sed -i "s#//pdo_query(\"SET time_zone ='+8:00'\")#pdo_query(\"SET time_zone ='+9:00'\")#" db_info.inc.php
+
+cd
+
+echo "/home/judge/src/web/include/db_info.inc.php edited!!"
+echo "Edit /home/judge/src/web/include/db_info.inc.php for more options!!"
 echo ""
-echo "Log in as admin and change new PW for admin!"
+echo "$OJNAME   admin ID : admin"
+echo "$OJNAME   admin PW : computerscience"
+echo ""
+echo "Log in as admin and change PW!"
+echo ""
+echo "Scripted by melongist"
+echo "Powered by CSL"
