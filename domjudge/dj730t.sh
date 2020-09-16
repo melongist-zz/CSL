d15272606f3779eccceebee846e1dfe484cf6c02
melongist <what_is_computer@msn.com>
2020-09-16 22:46:44 +0900

    Update dj730t.sh
    20.09.16

--- domjudge/dj730t.sh
+++ domjudge/dj730t.sh
@@ -50,42 +50,40 @@ sudo apt -y install libjsoncpp-dev
 
 
 cd $HOME/domjudge-7.3.0
-<<COMMENT
-sudo ./configure --prefix=$HOME/domjudge --with-domjudge-user=$USER --with-baseurl=BASEURL
-sudo smake domserver
-sudo make install-domserver
+#sudo ./configure --prefix=$HOME/domjudge --with-domjudge-user=$USER --with-baseurl=BASEURL
+#sudo smake domserver
+#sudo make install-domserver
 
-PASSWORD=$(sudo cat $HOME/domjudge/domserver/etc/initial_admin_password.secret)
+#PASSWORD=$(sudo cat $HOME/domjudge/domserver/etc/initial_admin_password.secret)
 
-cd $HOME/domjudge/domserver/bin
-sudo ./dj_setup_database install
+#cd $HOME/domjudge/domserver/bin
+#sudo ./dj_setup_database install
 
-sudo ln -s $HOME/domjudge/domserver/etc/apache.conf /etc/apache2/conf-available/domjudge.conf
-sudo ln -s $HOME/domjudge/domserver/etc/domjudge-fpm.conf /etc/php/7.4/fpm/pool.d/domjudge.conf
+#sudo ln -s $HOME/domjudge/domserver/etc/apache.conf /etc/apache2/conf-available/domjudge.conf
+#sudo ln -s $HOME/domjudge/domserver/etc/domjudge-fpm.conf /etc/php/7.4/fpm/pool.d/domjudge.conf
 
-sudo a2enmod proxy_fcgi setenvif rewrite
-sudo a2enconf php7.4-fpm domjudge
-sudo service php7.4-fpm reload
-sudo service apache2 reload
+#sudo a2enmod proxy_fcgi setenvif rewrite
+#sudo a2enconf php7.4-fpm domjudge
+#sudo service php7.4-fpm reload
+#sudo service apache2 reload
 
-cd
-sudo rm -f /var/www/html/index.html
-echo "<script>document.location=\"./domjudge/\";</script>" > index.html
-sudo chmod 644 index.html
-sudo chown root:root index.html
-sudo mv index.html /var/www/html/
+#cd
+#sudo rm -f /var/www/html/index.html
+#echo "<script>document.location=\"./domjudge/\";</script>" > index.html
+#sudo chmod 644 index.html
+#sudo chown root:root index.html
+#sudo mv index.html /var/www/html/
 
-sudo apt -y autoremove
+#sudo apt -y autoremove
 
-clear
+#clear
 
-echo "apache2 based"
-echo "domjudge 7.3.0 install completed!!"
-echo "Ver 2020.09.16"
-echo "Made by melongist(what_is_computer@msn.com)"
-echo "admin ID : admin"
-echo "admin PW : $PASSWORD"
-COMMENT
+#echo "apache2 based"
+#echo "domjudge 7.3.0 install completed!!"
+#echo "Ver 2020.09.16"
+#echo "Made by melongist(what_is_computer@msn.com)"
+#echo "admin ID : admin"
+#echo "admin PW : $PASSWORD"
 
 
 
