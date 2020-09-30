cb6c23ab533d0e71d8e82604cf737ce0da5ea342
melongist <what_is_computer@msn.com>
2020-10-01 01:29:06 +0900

    Update dj730t.sh
    20.10.01

--- domjudge/dj730t.sh
+++ domjudge/dj730t.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #for Ubuntu 20.04 LTS AWS Server and Desktop
 
-apt update
-apt -y upgrade
+sudo apt update
+sudo apt -y upgrade
 
 sudo apt -y install acl
 sudo apt -y install zip
 
-apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.5/ubuntu focal main'
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.5/ubuntu focal main'
 
-apt update
-apt -y upgrade
+sudo apt update
+sudo apt -y upgrade
 
-apt -y install mariadb-server mariadb-client
+sudo apt -y install mariadb-server mariadb-client
 
 echo ""
 echo "----------------------------------"
@@ -23,36 +23,36 @@ echo ""
 
 
 #root 패스워드 반드시 설정해야함. #1
-mysql_secure_installation
+sudo mysql_secure_installation
 
-apt -y install apache2
+sudo apt -y install apache2
 
-apt -y install php
-apt -y install php-fpm
+sudo apt -y install php
+sudo apt -y install php-fpm
 
-apt -y install php-gd
-apt -y install php-cli
-apt -y install php-intl
-apt -y install php-mbstring
-apt -y install php-mysql
-apt -y install php-curl
-apt -y install php-json
-apt -y install php-xml
-apt -y install php-zip
-apt -y install composer
-apt -y install ntp
+sudo apt -y install php-gd
+sudo apt -y install php-cli
+sudo apt -y install php-intl
+sudo apt -y install php-mbstring
+sudo apt -y install php-mysql
+sudo apt -y install php-curl
+sudo apt -y install php-json
+sudo apt -y install php-xml
+sudo apt -y install php-zip
+sudo apt -y install composer
+sudo apt -y install ntp
 
 wget https://www.domjudge.org/releases/domjudge-7.3.0.tar.gz
 tar xvf domjudge-7.3.0.tar.gz
 
-apt -y install build-essential
-apt -y install libcgroup-dev
-apt -y install libcurl4-openssl-dev
-apt -y install libjsoncpp-dev
+sudo apt -y install build-essential
+sudo apt -y install libcgroup-dev
+sudo apt -y install libcurl4-openssl-dev
+sudo apt -y install libjsoncpp-dev
 
 cd domjudge-7.3.0
-sudo ./configure --with-domjudge-user=$USER --with-baseurl=BASEURL
-sudo make domserver
+./configure --with-baseurl=BASEURL
+make domserver
 sudo make install-domserver
 
 PASSWORD=$(cat /opt/domjudge/domserver/etc/initial_admin_password.secret)
@@ -66,36 +66,7 @@ echo "Submit!! Mariadb's' root password!"
 echo "----------------------------------"
 echo ""
 # #1에서 설정한 root 패스워드 입력
-sudo ./dj_setup_database -u root -p -r install
+sudo ./dj_setup_database -u root -r install
 
-sudo ln -s /opt/domjudge/domserver/etc/apache.conf /etc/apache2/conf-available/domjudge.conf
-sudo ln -s /opt/domjudge/domserver/etc/domjudge-fpm.conf /etc/php/7.4/fpm/pool.d/domjudge.conf
 
-sudo a2enmod proxy_fcgi setenvif rewrite
-sudo systemctl restart apache2
 
-sudo a2enconf php7.4-fpm domjudge
-sudo systemctl reload apache2
-
-sudo service php7.4-fpm reload
-sudo service apache2 reload
-
-cd
-sudo rm -f /var/www/html/index.html
-echo "<script>document.location=\"./domjudge/\";</script>" > index.html
-sudo chmod 644 index.html
-sudo chown root:root index.html
-sudo mv index.html /var/www/html/
-
-apt -y autoremove
-
-PASSWORD=$(cat /opt/domjudge/domserver/etc/initial_admin_password.secret)
-
-clear
-
-echo "apache2 based"
-echo "domjudge 7.3.0 install completed!!"
-echo "Ver 2020.09.19"
-echo "Made by melongist(what_is_computer@msn.com)"
-echo "admin ID : admin"
-echo "admin PW : $PASSWORD"
