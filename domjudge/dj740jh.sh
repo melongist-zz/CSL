c6c6a49dc003f24f18675af0b4f4212c832a7e17
melongist <what_is_computer@msn.com>
2020-10-03 17:28:43 +0900

    Update dj740jh.sh

--- domjudge/dj740jh.sh
+++ domjudge/dj740jh.sh
@@ -29,15 +29,15 @@ sudo sed -i "s#GRUB_CMDLINE_LINUX_DEFAULT=\"\"#GRUB_CMDLINE_LINUX_DEFAULT=\"quie
 sudo sed -i "s#GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0 nvme_core.io_timeout=4294967295\"#GRUB_CMDLINE_LINUX_DEFAULT=\"quiet cgroup_enable=memory swapaccount=1\"#" /etc/default/grub.d/50-cloudimg-settings.cfg
 
 sudo update-grub
-echo "after : sudo reboot"
-echo "run   : sudo /opt/domjudge/judgehost/bin/dj_make_chroot"
-echo "run   : sudo /opt/domjudge/judgehost/bin/judgedaemon"
 
-#sudo reboot
+echo "run : sudo reboot"
+echo "run : sudo /opt/domjudge/judgehost/bin/dj_make_chroot"
+echo "run : setsid nohup /opt/domjudge/judgehost/bin/judgedaemon"
 
 #재부팅하고 나서 아래 명령어를 실행하면 저지호스트가 실행됨
+#sudo /opt/domjudge/judgehost/bin/dj_make_chroot    <- 이거는 한 번만 실행시키면 됨.
+#7.3.0 버전에서 dj_make_chroot 실행 안 될 때에는 ... sudo ./dj_make_chroot -D Ubuntu -R bionic
+#     /opt/domjudge/judgehost/bint/judgedaemon  를 실행시키면 저지데몬이 실행되면서 채점함.
+#setsid nohup /opt/domjudge/judgehost/bin/judgedaemon &   명령어를 EC2 터미널에서 실행시킨 후 종료 시키면 됨.
 
-#cd /opt/domjudge/judgehost/bin/
-#sudo ./dj_make_chroot
-#sudo ./dj_make_chroot -D Ubuntu -R bionic #이건 7.3.0 버전에서 dj_make_chroot 실행 안 될때...
-#./judgedaemon
+
