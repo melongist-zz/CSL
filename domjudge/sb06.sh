014e8bc956d8318176e943a355ca814d5ed771e9
melongist <what_is_computer@msn.com>
2020-10-10 12:29:03 +0900

    Update sb06.sh

--- domjudge/sb06.sh
+++ domjudge/sb06.sh
@@ -21,7 +21,23 @@ tar -xvf spotboard-webapp-0.6.0.tar.gz
 mv spotboard-webapp-0.6.0 spotboard
 sudo mv spotboard /var/www/html/
 
-sed -i "s#feed_server_path = \'./sample/\'#feed_server_path = \'./\'#" /var/www/html/spotboard/config.js
-sed -i "s#\'award_slide.json\' :\'./sample/award_slide.json\'#\'award_slide.json\' :\'./award_slide.json\'#" /var/www/html/spotboard/config.js
+sed -i "s#feed_server_path = './sample/'#feed_server_path = './'#" /var/www/html/spotboard/config.js
+sed -i "s#'award_slide.json' :'./sample/award_slide.json'#'award_slide.json' :'./award_slide.json'#" /var/www/html/spotboard/config.js
 sed -i "s#animation          : false#animation          : true#" /var/www/html/spotboard/config.js
+
+echo "" | tee -a domjudge.txt
+echo "spotboard for domjudge installed!!" | tee -a domjudge.txt
+echo "Ver 2020.10.09" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "Check spotboard!" | tee -a domjudge.txt
+echo "------" | tee -a domjudge.txt
+echo "http:/localhost/spotboard/" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "configuration for spotboard" | tee -a domjudge.txt
+echo "check /var/www/html/spotboard/config.js" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "Next step : install spotboard-converter" | tee -a domjudge.txt
+echo ""
+echo ""
+
 
