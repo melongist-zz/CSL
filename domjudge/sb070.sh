ec5e1e49f28dec1f2207ef01da5e8a732db36db0
melongist <what_is_computer@msn.com>
2020-10-19 19:11:08 +0900

    Update sb070.sh

--- domjudge/sb070.sh
+++ domjudge/sb070.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#spotboard 0.6.0 for domjudge7.4.0.dev + Ubuntu 20.04 LTS Server
+#spotboard 0.7.0 for domjudge7.4.0.dev + Ubuntu 20.04 LTS Server
 
 #terminal commands to install spotboard webapp
-#wget https://raw.githubusercontent.com/melongist/CSL/master/domjudge/sb06.sh
-#bash sb06.sh
+#wget https://raw.githubusercontent.com/melongist/CSL/master/domjudge/sb070.sh
+#bash sb070.sh
 
 #------
 #spotboard for domjudge
@@ -12,18 +12,23 @@ cd
 sudo apt update
 sudo apt -y upgrade
 
-sudo apt -y install nodejs
-sudo apt -y install npm
-
 #https://github.com/spotboard/spotboard
 wget https://raw.githubusercontent.com/melongist/CSL/master/domjudge/spotboard-webapp-0.7.0.tar.gz
 tar -xvf spotboard-webapp-0.7.0.tar.gz
 mv spotboard-webapp-0.7.0 spotboard
 sudo mv spotboard /var/www/html/
+cd /var/www/html/spotboard/src/
 
-sed -i "s#feed_server_path = './sample/'#feed_server_path = './src/'#" /var/www/html/spotboard/src/config.js
-sed -i "s#'award_slide.json' :'./sample/award_slide.json'#'award_slide.json' :'./src/award_slide.json'#" /var/www/html/spotboard/src/config.js
-sed -i "s#animation          : false#animation          : true#" /var/www/html/spotboard/src/config.js
+sudo apt -y install nodejs
+sudo apt -y install npm
+sudo npm install -g npm
+sudo npm install -g grunt-cli
+sudo npm run build
+
+
+sed -i "s#feed_server_path = './src/'#feed_server_path = './dist/'#" /var/www/html/spotboard/dist/config.js
+sed -i "s#'award_slide.json' :'./src/award_slide.json'#'award_slide.json' :'./dist/award_slide.json'#" /var/www/html/spotboard/dist/config.js
+sed -i "s#animation          : false#animation          : true#" /var/www/html/spotboard/dist/config.js
 
 clear
 
@@ -33,10 +38,10 @@ echo "Ver 2020.10.19" | tee -a domjudge.txt
 echo "" | tee -a domjudge.txt
 echo "Check spotboard!" | tee -a domjudge.txt
 echo "------" | tee -a domjudge.txt
-echo "http:/localhost/spotboard/src/" | tee -a domjudge.txt
+echo "http:/localhost/spotboard/dist/" | tee -a domjudge.txt
 echo "" | tee -a domjudge.txt
 echo "configuration for spotboard" | tee -a domjudge.txt
-echo "check /var/www/html/spotboard/src/config.js" | tee -a domjudge.txt
+echo "check /var/www/html/spotboard/dist/config.js" | tee -a domjudge.txt
 echo "" | tee -a domjudge.txt
 echo "Next step : install domjudge-converter" | tee -a domjudge.txt
 echo ""
