cf29b02d6a4580f7b49b870a4f6082189746fe0a
melongist <what_is_computer@msn.com>
2021-02-12 22:58:28 +0900

    2021.02.12 update
    2021.02.12 update

--- moodle/moodle210212.sh
+++ moodle/moodle210212.sh
@@ -22,62 +22,54 @@ sudo apt update
 sudo apt -y upgrade
 
 
+sudo apt install -y nginx
+sudo systemctl is-enabled nginx
 
-#https://docs.moodle.org/310/en/Step-by-step_Installation_Guide_for_Ubuntu
-for pkg in apache2 mysql-client mysql-server php libapache2-mod-php graphviz aspell ghostscript clamav php7.4-pspell php7.4-curl php7.4-gd php7.4-intl php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-ldap php7.4-zip php7.4-soap php7.4-mbstring git
-do
-  while ! sudo apt install -y "$pkg" 
-  do
-    echo "Network fail, retry... you might want to change another apt source for install"
-  done
-done
+sudo apt install -y mariadb-server mariadb-client
+sudo systemctl is-enabled mariadb
 
-sudo service apache2 restart
+sudo mysql_secure_installation
 
-cd /opt
-
-sudo git clone git://git.moodle.org/moodle.git
-
-cd moodle
-
-sudo git branch -a
-
-sudo git branch --track MOODLE_39_STABLE origin/MOODLE_39_STABLE
-
-sudo git checkout MOODLE_39_STABLE
-
-sudo cp -R /opt/moodle /var/www/html/
-
-sudo mkdir /var/moodledata
-
-sudo chown -R www-data /var/moodledata
-
-sudo chmod -R 777 /var/moodledata
-
-sudo chmod -R 0755 /var/www/html/moodle
+#sudo mysql -u root -p
 
 
-sudo sed -i -e '$a\\' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo sed -i -e '$a#by ${THISFILE} installation' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo sed -i -e '$adefault_storage_engine = innodb' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo sed -i -e '$ainnodb_file_per_table = 1' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo sed -i -e '$ainnodb_file_format = Barracuda' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo apt install -y php php-mysql php-fpm
+sudo systemctl is-enabled php7.4-fpm
 
-
-sudo service mysql restart
-
-DBUSER=$(grep user /etc/mysql/debian.cnf|head -1|awk  '{print $3}')
-PASSWORD=$(grep password /etc/mysql/debian.cnf|head -1|awk  '{print $3}')
-
+sudo apt install php-common php-iconv php-curl php-mbstring php-xmlrpc php-soap php-zip php-gd php-xml php-intl php-json libpcre3 libpcre3-dev graphviz aspell ghostscript clamav
 
 wget https://raw.githubusercontent.com/melongist/CSL/master/moodle/moodledb.sql
-
-sudo mysql -h localhost -u"$DBUSER" -p"$PASSWORD" < moodledb.sql
+sudo mysql -u root -p < moodledb.sql
 sudo rm moodledb.sql
 
-sudo chmod -R 777 /var/www/html/moodle
+wget -c https://download.moodle.org/download.php/direct/stable310/moodle-latest-310.zip
+sudo unzip moodle-latest-310.zip -d /var/www/html/
 
-#sudo chmod -R 0755 /var/www/html/moodle
+sudo chown www-data:www-data -R /var/www/html/moodle
+sudo chmod 775 -R /var/www/html/moodle
+
+sudo mkdir -p /var/moodledata
+sudo chmod 775 -R /var/moodledata
+sudo chown www-data:www-data -R  /var/moodledata
+
+cd /var/www/html/moodle/
+sudo cp config-dist.php config.php
+
+sudo sed -i "s/$CFG->dbtype    = 'pgsql';/$CFG->dbtype    = 'mariadb';/" /var/www/html/moodle/config.php
+sudo sed -i "s/$CFG->dbuser    = 'username';/$CFG->dbuser    = 'moodleadmin';/" /var/www/html/moodle/config.php
+sudo sed -i "s/$CFG->dbpass    = 'password';/$CFG->dbpass    = 'Secur3P@zzwd';/" /var/www/html/moodle/config.php
+sudo sed -i "s/$CFG->wwwroot   = 'http:\/\/example.com\/moodle';/$CFG->wwwroot   = 'http:\/\/learning.testprojects.me';/" /var/www/html/moodle/config.php
+sudo sed -i "s/$CFG->dataroot  = '\/home\/example\/moodledata';/$CFG->dataroot  = '\/var\/moodledata';/" /var/www/html/moodle/config.php
+
+cd
+wget https://raw.githubusercontent.com/melongist/CSL/master/moodle/moodle.conf
+sudo cp moodle.conf /etc/nginx/conf.d/moodle.conf
+sudo chown root:root /etc/nginx/conf.d/moodle.conf
+sudo chmod 644 /etc/nginx/conf.d/moodle.conf
+sudo rm moodle.conf
+
+sudo nginx -t
+sudo systemctl reload nginx
 
 
 
