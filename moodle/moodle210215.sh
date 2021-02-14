d0651914f5a1a460b5793cb447cc93ebaa64e57e
melongist <what_is_computer@msn.com>
2021-02-15 02:43:30 +0900

    2021.02.15 update
    2021.02.15 update

--- moodle/moodle210215.sh
+++ moodle/moodle210215.sh
@@ -34,7 +34,7 @@ sudo apt install -y graphviz aspell ghostscript clamav php7.4-pspell php7.4-curl
 
 
 sudo sed -i "s:memory_limit = 128M:memory_limit = 256M:g" /etc/php/7.4/fpm/php.ini
-sudo sed -i "s:upload_max_filesize = 2M:upload_max_filesize = 64M:g" /etc/php/7.4/fpm/php.ini
+sudo sed -i "s:upload_max_filesize = 2M:upload_max_filesize = 16M:g" /etc/php/7.4/fpm/php.ini
 
 sudo sed -i "s:index index.html:index index.php index.html:g" /etc/nginx/sites-enabled/default
 sudo sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
@@ -42,7 +42,7 @@ sudo sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enable
 sudo sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
 sudo sed -i "s|# deny access to .htaccess files|}\n\n\n\t# deny access to .htaccess files|g" /etc/nginx/sites-enabled/default
 
-#for nginx moodle css/js
+#for nginx's moodle css/js
 sudo sed -i "s#server_name _;#server_name _;\n\n\tlocation ~ [^/]\\.php(/|$) {\n\t\tfastcgi_split_path_info  ^(.+\\.php)(/.+)$;\n\t\tfastcgi_index            index.php;\n\t\tfastcgi_pass             unix:/var/run/php/php7.4-fpm.sock;\n\t\tinclude                  fastcgi_params;\n\t\tfastcgi_param   PATH_INFO       \$fastcgi_path_info;\n\t\tfastcgi_param   SCRIPT_FILENAME \$document_root\$fastcgi_script_name;\n\t}#g" /etc/nginx/sites-enabled/default
 
 sudo systemctl restart nginx
@@ -52,8 +52,8 @@ sudo apt install -y git
 cd /opt
 sudo git clone git://git.moodle.org/moodle.git
 cd moodle
-sudo git branch --track MOODLE_311_STABLE origin/MOODLE_311_STABLE
-sudo git checkout MOODLE_311_STABLE
+sudo git branch --track MOODLE_310_STABLE origin/MOODLE_310_STABLE
+sudo git checkout MOODLE_310_STABLE
 
 cd
 sudo cp -R /opt/moodle /var/www/html/
@@ -66,15 +66,74 @@ sudo chmod -R 0755 /var/www/html/moodle
 sudo sed -i "s|# pid-file|innodb_file_per_table = 1\n# pid-file|g" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo service mysql restart
 
+
+
+#moodledb.sql edit
+#for moodle db user
 wget https://raw.githubusercontent.com/melongist/CSL/master/moodle/moodledb.sql
+
+DBUSER="o"
+INPUTS="x"
+while [ ${DBUSER} != ${INPUTS} ]; do
+  echo -n "Enter  DBUSER name : "
+  read DBUSER
+  echo -n "Repeat DBUSER name : "
+  read INPUTS
+done
+sudo sed -i "s|moodleuser|${DBUSER}|g" ./moodledb.sql
+
+DBPASS="o"
+INPUTS="x"
+while [ ${DBPASS} != ${INPUTS} ]; do
+  echo -n "Enter  DBUSER password : "
+  read DBPASS
+  echo -n "Repeat DBUSER password : "
+  read INPUTS
+done
+sudo sed -i "s|passwordformoodleuser|${DBPASS}|g" ./moodledb.sql
+
 sudo mysql -u root -p < moodledb.sql
 sudo rm moodledb.sql
 
 sudo chmod -R 777 /var/www/html/moodle
 
 
+#curl installation
+sudo apt -y install curl
 
+#Identifing AWS Ubuntu 20.04 LTS
+if [ -f /etc/default/grub.d/50-cloudimg-settings.cfg ]; then
+  SERVERTYPES="AWS SERVER"
+  IPADDRESS=($(curl http://checkip.amazonaws.com))
+else
+  SERVERTYPES="LOCAL SERVER"
+  IPADDRESS=($(hostname -I))
+fi
 
+clear
 
-
-
+echo ""
+echo "--- moodle installed ---"
+echo ""
+echo "Next : web installaion needed..."
+echo ""
+echo "$SERVERTYPES"
+echo "http://${IPADDRESS[0]}"
+echo "moodle html home directory : /var/www/html/moodle/"
+echo "moodle data directory :      /var/moodledata/"
+echo ""
+echo "If you want to use domain name your own?"
+echo "- First of all! Connect you domain name to ${IPADDRESS[0]}."
+echo "- And then install with web installation at http://domainname/moodle/"
+echo ""
+echo "If you don't have any domain name?"
+echo "- Just install with web installation at http://${IPADDRESS[0]}/moodle/"
+echo ""
+echo "This Script is supported by CSL(South Korea CSTA)"
+echo ""
+echo ""
+echo "After installation completed : "
+echo "You must change mod with :"
+echo "sudo chmod -R 755 /var/www/html/moodle"
+echo ""
+echo ""
--- moodle/moodledb.sql
+++ moodle/moodledb.sql
@@ -3,5 +3,3 @@ CREATE DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE user 'moodleuser'@'localhost' IDENTIFIED BY 'passwordformoodleuser';
 
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle.* TO 'moodleuser'@'localhost';
-
-quit;
