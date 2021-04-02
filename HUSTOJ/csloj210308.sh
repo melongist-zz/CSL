792261b7a248e79aeda5c51ff7c96ca71dcf255c
melongist <what_is_computer@msn.com>
2021-04-03 05:35:49 +0900

    2021.04.03 update
    2021.04.03 update

--- HUSTOJ/csloj210308.sh
+++ HUSTOJ/csloj210308.sh
@@ -195,9 +195,6 @@ reset
 
 
 
-
-cd
-
 #db_info.inc.php edit
 #for OJ NAME
 clear
@@ -513,18 +510,32 @@ sed -i "s/release YY.MM.DD/release ${VER_DATE}/" /home/judge/src/web/admin/msg.t
 sed -i "s#KB</div># KB</div>#" /home/judge/src/web/status.php
 sed -i "s#ms</div># ms</div>#" /home/judge/src/web/status.php
 
-
 #for python judging error patch
 sed -i "s/OJ_PYTHON_FREE=0/OJ_PYTHON_FREE=1/" /home/judge/etc/judge.conf
 ​
+
+#for cslojmaintenance
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/cslojmaintenance.sh -O /home/${SUDO_USER}/cslojmaintenance.sh
+chown ${SUDO_USER}:${SUDO_USER} /home/${SUDO_USER}/cslojmaintenance.sh
+
+if [ -e "/var/spool/cron/crontabs/root" ]; then
+  if grep "cslojmaintenance.sh" /var/spool/cron/crontabs/root ; then
+    sed -i '/cslojmaintenance.sh/d' /var/spool/cron/crontabs/root
+  fi
+fi
+
+crontab -l > temp
+echo '30 4 * * * sudo bash /home/'${SUDO_USER}'/cslojmaintenance.sh' >> temp
+crontab temp
+rm -f temp
+
+
+
 #temporary until next HUSTOJ release
 sed -i "s/318,17,41,42,72,99,217/318,17,41,42,49,72,99,217/" /home/judge/src/core/judge_client/okcalls64.h
 sed -i "s/99,158,231,275,292,511/99,158,186,231,234,268,275,292,511/" /home/judge/src/core/judge_client/okcalls64.h
 sed -i "s/11,12,20,21,59,63,89,99/11,12,14,20,21,39,59,63,89,99/" /home/judge/src/core/judge_client/okcalls64.h
-
-
 cd /home/judge/src/core/
-
 bash make.sh
 
 
