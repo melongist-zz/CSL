85a43ca2b2aac8491ff0da74e1c2f1c2c7bd437f
melongist <what_is_computer@msn.com>
2021-05-08 10:39:28 +0900

    2021.05.08 update
    2021.05.08 update

--- moodle/moodle210508.sh
+++ moodle/moodle210508.sh
@@ -62,8 +62,8 @@ sudo sed -i "s|# If MySQL is running as|\[mysql\]\ndefault-character-set = utf8m
 sudo systemctl restart mysql
 
 #php
+sudo add-apt-repository ppa:ondrej/php
 sudo apt install -y php php-fpm php-cli php-mysql php-gd php-imagick php-tidy php-xmlrpc
-
 sudo apt install -y graphviz aspell ghostscript clamav php7.4-pspell php7.4-gd php7.4-intl php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-ldap php7.4-zip php7.4-soap php7.4-mbstring
 
 sudo sed -i "s:memory_limit = 128M:memory_limit = 256M:g" /etc/php/7.4/fpm/php.ini
@@ -88,8 +88,6 @@ sudo systemctl restart nginx
 sudo systemctl restart mysql
 sudo systemctl restart php7.4-fpm
 sudo systemctl restart php7.4-fpm.service
-
-
 
 
 #original git
@@ -160,7 +158,7 @@ fi
 clear
 
 echo ""
-echo "--- moodle installed ---"
+echo "--- MOODLE_310_STABLE installed ---"
 echo ""
 echo "Next : web installaion needed..."
 echo ""
