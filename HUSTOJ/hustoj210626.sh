509505cfe7688b61f01205d91a968d3eb4478f83
melongist <what_is_computer@msn.com>
2021-08-09 06:55:24 +0900

    21.08.09 update
    21.08.09 update

--- HUSTOJ/hustoj210626.sh
+++ HUSTOJ/hustoj210626.sh
@@ -32,10 +32,15 @@ timedatectl set-timezone 'Asia/Seoul'
 
 apt update
 apt -y upgrade
+apt -y install apt-transport-https ca-certificates curl gnupg lsb-release subversion zip unzip
 apt -y autoremove
-apt -y install subversion
-apt -y install zip
-apt -y install unzip
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt -y install docker-ce docker-ce-cli containerd.io
+
+
 
 /usr/sbin/useradd -m -u 1536 judge
 cd /home/judge/ || exit
